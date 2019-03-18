9fa23eb63a062b41e23369df9964983e94504de1
Edward <lued2018@gmail.com>
2019-03-17 22:20:20 -0400

    added text field effects

---
+++ augmented-ID_iOS/augmented-identity/Controllers/SignUpViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpViewController.swift
+//  augmented-identity
+//
+//  Created by Edward on 3/17/19.
+//  Copyright © 2019 Edward. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var email: HoshiTextField!
+    @IBOutlet weak var password: HoshiTextField!
+    @IBOutlet weak var confirmation: HoshiTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func signUpAction(_ sender: Any) {
+        
+    }
+}
