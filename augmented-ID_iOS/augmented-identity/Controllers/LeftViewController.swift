37e7dc5f6b484b3780c89e105c7c778755ea01c9
Edward <lued2018@gmail.com>
2019-04-03 13:08:19 -0400

    edited tableview

--- augmented-ID_iOS/augmented-identity/Controllers/LeftViewController.swift
+++ augmented-ID_iOS/augmented-identity/Controllers/LeftViewController.swift
@@ -7,10 +7,27 @@
 //
 
 import UIKit
+import Firebase
 
 class LeftViewController: UIViewController {
-
+    
+    var users: [NSDictionary] = []
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.ref = Database.database().reference()
+        self.ref.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let value = snapshot.value as? NSDictionary {
+                for each in value {
+                    let dict = each.1 as? NSDictionary
+                    self.users.append(dict!)
+                }
+            }
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
+    
 }
---
+++ augmented-ID_iOS/augmented-identity/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  augmented-identity
+//
+//  Created by Edward on 4/3/19.
+//  Copyright © 2019 Edward. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
